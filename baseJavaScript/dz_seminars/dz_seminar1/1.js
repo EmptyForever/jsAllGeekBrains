ffc10bcafe825ad6cee25db395987590b9dfd8e9
Aleks <kakdela208@mail.ru>
2023-10-28 19:37:18 +1000

    add allinfo (lec 2< sem 2< sz2)

--- baseJavaScript/dz_seminars/dz_seminar1/1.js
+++ baseJavaScript/dz_seminars/dz_seminar1/1.js
@@ -2,10 +2,11 @@
 
 let gradusCels = prompt("Введите градусы цельсия: ");
 function celcInFarengent(gradCels) {
-    let gradFareng;
-    gradFareng = (9 / 5) * gradCels + 32;
-    return gradFareng;
+  let gradFareng;
+  gradFareng = (9 / 5) * gradCels + 32;
+  return gradFareng;
 }
 let gradusFarengeit = celcInFarengent(gradusCels);
+let tempik = Number(gradusFarengeit.toFixed(2));
 
-alert(`Цельсий: ${gradusCels}, Фаренгейт: ${gradusFarengeit}`);
+alert(`Цельсий: ${gradusCels}, Фаренгейт: ${tempik}`);
